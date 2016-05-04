232aeddebbb76d4519e4bdaebeb312be5f490a32
MsEmma <emmaphiri@hotmail.com>
2016-05-04 17:48:01 +0200

    added database

--- database.sql
+++ database.sql
@@ -1,31 +1,36 @@
-use golden_girls;
+use Goal_it;
 
+
+DROP TABLE IF EXISTS `Milestones`;
+DROP TABLE IF EXISTS `Goals`;
 DROP TABLE IF EXISTS `Members`;
-  create table Members(
+
+
+create table members(
    	members_id int not null auto_increment,
       user_name VARCHAR(50),
 	     primary key(members_id),
 	      constraint uc_user_name unique (user_name)
 );
 
-DROP TABLE IF EXISTS `Goals`;
-  create table Goals(
-    Goals_id int not null auto_increment,
+
+create table goals(
+    goals_id int not null auto_increment,
 	   members_id int,
 	    goal_name VARCHAR(50),
 	     start_date date not null,
 	      target_date date not null,
-	       primary key(Goals_id),
-	        foreign key(members_id) REFERENCES Members(members_id),
+	       primary key(goals_id),
+	        foreign key(members_id) REFERENCES members(members_id),
 	         constraint uc_goal_name unique (goal_name)
 );
 
-DROP TABLE IF EXISTS 'Milestones';
-  create table Milestones(
+
+create table milestones(
 	    id int not null auto_increment,
-	     Goals_id int,
-	      Milestone_name VARCHAR(50),
+	     goals_id int,
+	      milestone VARCHAR(50),
 	       primary key(id),
-	        foreign key(Goals_id) REFERENCES Goals(Goals_id),
-	         constraint uc_Milestone_name unique (Milestone_name)
+	        foreign key(goals_id) REFERENCES goals(goals_id),
+	         constraint uc_milestone unique (milestone)
 );
