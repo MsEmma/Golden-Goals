8c55e614776743530c1d9789873c0f71d375f7f9
Thaskoo <thandokazi@projectcodex.co>
2016-05-05 17:43:55 +0200

    changes made

--- goal.sql
+++ goal.sql
@@ -1,2 +1,2 @@
 INSERT INTO goals (member_id, goal, start_date, target_date) VALUES (1, 'Eat less meat & eat more vegetables', '1 May 2016', '31 July 2016');
-INSERT INTO milestones(goal_id, milestone, timeframe) VALUES ( 1, 'Complete 4 meat free Mondays', 4 );
+INSERT INTO milestones (goal_id, milestone, timeframe) VALUES (1, 'Complete 4 meat free Mondays', 4);
--- routes/notifications.js
+++ routes/notifications.js
@@ -11,18 +11,21 @@ exports.show = function(req, res, next) {
     });
 };
 
-// exports.update = function(req, res, next) {
-//
-//     var data = {
-//         milestone : req.body.milestone,
-//         personal_rating: Number(req.body.personal_rating)
-//     };
-//     var id = req.params.id;
-//     req.getConnection(function(err, connection) {
-//         if (err) return next(err);
-//         connection.query('UPDATE milestones SET ? WHERE id = ?', [data, id], function(err, rows) {
-//             if (err) return next(err);
-//             res.redirect('/');
-//         });
-//     });
-// };
+exports.update = function(req, res, next) {
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+    connection.query('UPDATE milestones SET ? WHERE id = ?', [data, id], function(err, rows){
+      
+      var data = {
+            milestone : req.body.milestone,
+            personal_rating: Name(req.body.personal_rating)
+      };
+
+          console.log(data);
+    });
+
+    if (err) return next(err);
+    res.render('/');
+});
+
+};
