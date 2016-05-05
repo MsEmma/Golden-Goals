1378d7167cccbdf99e07070949c8d5319db51c09
MsEmma <emmaphiri@hotmail.com>
2016-05-05 18:08:18 +0200

    pics

--- routes/notifications.js
+++ routes/notifications.js
@@ -10,19 +10,3 @@ exports.show = function(req, res, next) {
             });
     });
 };
-
-// exports.update = function(req, res, next) {
-//
-//     var data = {
-//         milestone : req.body.milestone,
-//         personal_rating: Number(req.body.personal_rating)
-//     };
-//     var id = req.params.id;
-//     req.getConnection(function(err, connection) {
-//         if (err) return next(err);
-//         connection.query('UPDATE milestones SET ? WHERE id = ?', [data, id], function(err, rows) {
-//             if (err) return next(err);
-//             res.redirect('/');
-//         });
-//     });
-// };
